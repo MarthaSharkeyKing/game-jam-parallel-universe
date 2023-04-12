710d77bc832c923c1370be6a9f25ecb2457b56ba
Elliott Reid (He/Him) <ereid@scottlogic.com>
2023-04-12 18:52:59 +0100

    Add scorecard

--- find-parralell/src/App.js
+++ find-parralell/src/App.js
@@ -1,13 +1,16 @@
 import "./App.css";
 import { CountdownTimer } from "./Timer/CountdownTimer";
 import Grid from "./Grid/Grid";
+import { Scorecard } from "./Scorecard/Scorecard";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">Close the Parralell universes!</header>
       <body>
-        <div className="Menu"></div>
+        <div className="Menu">
+          <Scorecard currentScore={10} />
+        </div>
         <div className="Game">
           <CountdownTimer initialSeconds={60} />
           <Grid />
